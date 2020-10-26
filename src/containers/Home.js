babaded3f64f10b68f91416fdde1c205022c5bcf
laraferreri <lbf287@nyu.edu>
2020-10-26 17:43:29 -0400

    sve

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 function Home() {
+    useEffect(() => {
+        axios
+        .get(
+            `https://quote-garden.herokuapp.com/api/v2/genres/${}?page=1&limit=10`
+        )
+        .then()
+        .catch()
+    }, []);
+
     return(
         <div>
         <h1>Home: Midterm</h1>
         </div>
     );
 }
+
+
 
 export default Home;
