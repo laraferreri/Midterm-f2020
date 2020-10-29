5eefdf9271579458b15d57ea2e8e60256e51d37e
laraferreri <lbf287@nyu.edu>
2020-10-29 02:58:11 -0400

    save3

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,18 +4,18 @@ import axios from "axios";
 import Header from "../components/Header";
 
 const apiKey = '563492ad6f917000010000015390cedee23740efb1d9578d11aaa935';
+const apiKey2 = 'b99SVmlnOXbWXBjYOfs1sXuGAyLreZbZ5';
 
 
 function Home() {
 const [searchParam, setSearchParam] = useState("nature");
 const [pexelData, setPexelData] = useState([]);
-const [gifData, setGifData] = useState();
-const apiKey2 = 'b99SVmlnOXbWXBjYOfs1sXuGAyLreZbZ5';
-// const search = `https://api.giphy.com/v1/gifs/search?query=${searchParam}&apiKey=${apiKey2}`;
+const [searchParam2, setSearchParam2] = useState("nature");
+const [gifData, setGifData] = useState([]);
 
 useEffect(() => {
     axios
-    .get(`https://api.pexels.com/v1/search?query=${searchParam}&apiKey=${apiKey}`,
+    .get(`https://api.pexels.com/v1/search?query=${searchParam2}&apiKey=${apiKey}`,
     {
             headers: {
                 'Authorization': apiKey
@@ -31,68 +31,57 @@ useEffect(() => {
         console.warn(error);
     })
 }, [searchParam]);
-// console.log(pexelData);
+console.log(pexelData);
 
 useEffect(() => {
 axios
-.get(`https://api.giphy.com/v1/stickers/packs/3138/stickers?api_key=${apiKey2}`
+.get(`https://api.giphy.com/v1/gifs/search?query=${searchParam2}&apiKey=${apiKey2}`,
 )
 
 .then(function (response){
 console.log(response);
-const data = response
-setGifData(data)
+const data2 = response
+setGifData(data2)
 })
 
 .catch(function(error){
 console.warn(error);
 })
 
-}, [gifData]);
-
+}, [searchParam2]);
 
 
 return (
     <>
     <Header/>
         <main className= "home">
+
+                <div className= "description">
+                Click one to view images and GIFS of that topic...
+                </div>
+
             <div className= "topics">
-            <nav>
                 <div onClick= {() => setSearchParam("dogs")}> dogs </div>
                 <div onClick= {() => setSearchParam("flower")}> flower </div>
                 <div onClick= {() => setSearchParam("house")}> house </div>
                 <div onClick= {() => setSearchParam("people")}> people </div>
                 <div onClick= {() => setSearchParam("cats")}> cats </div>
                 <div onClick= {() => setSearchParam("nature")}> nature </div>
-            </nav>
             </div>
-
-            <div className= "text">
-                <div className= "description">
-                blah blah blah blah
-                </div>
-                {}
-                <div className="info">
-                <h2> blah blah blah</h2>
-                </div>
-                
-                <div className= "topic">
-                <p>{searchParam}</p>
-                </div>
-
-             </div>
         </main>
+
+        <div className="info">
 
         {pexelData.map((photo, i) => (
         <div className="Photo" key={i}>
 
           <img src={photo.url} alt="alt text" />
           <h3>{photo.photographer}</h3>
-          <h4>{photo.url}</h4>
-          <h5>{photo.id}</h5>
-          
+          <h4><a href={photo.url}> Click here for link!</a></h4>
+          <h5> photo id number:{photo.id}</h5>
         </div>
       ))}
+      </div>
      </>
     );
 }
